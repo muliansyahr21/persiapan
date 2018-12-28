82c1a646e1028ba34bec653b40d47fe7ca8e68cd
muliansyahr21 <44266749+muliansyahr21@users.noreply.github.com>
2018-12-29 02:32:47 +0700

    cek2

--- ams_native-master/ams_native-master/include/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host     = "localhost";    // Nama host
-$username = "root";         // Username database
-$password = "masrud.com";   // Password database
-$database = "ams_native";   // Nama database
